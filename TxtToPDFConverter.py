7e68541ee9cbfee02061a71262674c9c28374321
jani-e <j.eriksson91@gmail.com>
2022-10-19 16:33:10 +0300

    Muotoiltu python koodi

--- TxtToPDFConverter.py
+++ TxtToPDFConverter.py
@@ -1,10 +1,12 @@
+# -*- coding: cp1252 -*-
+
 from fpdf import FPDF
 
 def convert_txt_to_pdf(input_file, output_file):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size = 10)
-    f = open(input_file, "r")
-    for x in f:
-        pdf.cell(190, 10, txt = x, ln = 1, align = 'C')
+    pdf.set_font("Helvetica", size = 10)
+    text_file = open(input_file, "r")
+    for row in text_file:
+        pdf.cell(190, 10, txt = row, ln = 1, align = 'C')
     pdf.output(output_file)
